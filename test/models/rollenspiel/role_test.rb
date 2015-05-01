f8697bac762e1c262928d8b5e91166f3b9dc00b3
Jens Bissinger <mail@jens-bissinger.de>
2015-05-01 22:55:24 +0200

    Add test.

--- test/models/rollenspiel/role_test.rb
+++ test/models/rollenspiel/role_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 module Rollenspiel
   class RoleTest < ActiveSupport::TestCase
     test "creates and inherits" do
-      r = Rollenspiel::Role.create! name: 'manager'
+      r = Role.create! name: 'manager'
       r.inherit! Role.new(name: 'read')
 
       assert r.inherited? 'read'
@@ -15,6 +15,26 @@ module Rollenspiel
     test "knows its provider" do
       o = TestOrganization.create!
       assert o.role(:read).provider
+    end
+
+    test "destroys when inherited to role is destroyed" do
+      r1 = Role.create! name: "one"
+      r2 = Role.create!(name: "two")
+      r1.inherit! r2
+
+      assert_difference("RoleInheritance.count", -1) do
+        r1.destroy!
+      end
+    end
+
+    test "destroys when inherited from role is destroyed" do
+      r1 = Role.create! name: "one"
+      r2 = Role.create!(name: "two")
+      r1.inherit! r2
+
+      assert_difference("RoleInheritance.count", -1) do
+        r2.destroy!
+      end
     end
   end
 end
