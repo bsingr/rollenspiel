6701eb0fd2a694e8926d256a9828491128ecac2b
Jens Bissinger <mail@jens-bissinger.de>
2015-05-01 16:46:28 +0200

    Add tests.

--- test/models/rollenspiel/role_test.rb
+++ test/models/rollenspiel/role_test.rb
@@ -5,8 +5,16 @@ module Rollenspiel
     test "creates and inherits" do
       r = Rollenspiel::Role.create! name: 'manager'
       r.inherit! Role.new(name: 'read')
+
       assert r.inherited? 'read'
       assert_not r.inherited? 'foo'
+
+      assert_not r.provider
+    end
+
+    test "knows its provider" do
+      o = TestOrganization.create!
+      assert o.role(:read).provider
     end
   end
 end
