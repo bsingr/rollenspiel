2e39ed7f1790adc2b41f1bf5230d0d63f2c39feb
Jens Bissinger <mail@jens-bissinger.de>
2015-05-01 19:11:48 +0200

    Improve error messaging.

--- app/models/rollenspiel/role_provider/role_builder.rb
+++ app/models/rollenspiel/role_provider/role_builder.rb
@@ -9,6 +9,10 @@ module Rollenspiel
         def build role_name
           Rollenspiel::Role.find_or_initialize_by name: role_name, provider_type: @class_name
         end
+
+        def to_s
+          "#{self.class}(#{@class_name})"
+        end
       end
 
       class InstanceRoleBuilder
@@ -18,6 +22,10 @@ module Rollenspiel
 
         def build role_name
           @record.provided_roles.build name: role_name
+        end
+
+        def to_s
+          "#{self.class}(#{@record.inspect})"
         end
       end
 
@@ -29,15 +37,20 @@ module Rollenspiel
       def build
         roles_by_key = {}
         @structure.layout[:roles].each do |role_or_name|
-          if role_or_name.kind_of?(Role)
+          if role_or_name.respond_to?(:id)
             roles_by_key[role_or_name] = role_or_name
           else
             roles_by_key[role_or_name] = @role_builder.build(role_or_name)
           end
         end
         @structure.layout[:inheritances].each do |role_or_name, inherits_roles|
-          inherits_roles.each do |inherits_role|
-            roles_by_key[role_or_name].inherit roles_by_key[inherits_role]
+          inherits_roles.each do |inherits_role_or_name|
+            raise ArgumentError, "Role #{@role_builder} #{role_or_name} is not available" unless roles_by_key[role_or_name]
+            if inherits_role_or_name.respond_to?(:id)
+              roles_by_key[role_or_name].inherit inherits_role_or_name
+            else
+              roles_by_key[role_or_name].inherit roles_by_key[inherits_role_or_name]
+            end
           end
         end
         @structure.layout[:callbacks][:on_grant].each do |role_or_name, role_callback|
