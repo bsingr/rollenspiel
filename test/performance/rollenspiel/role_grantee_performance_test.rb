8a817ab152894e13744c260d5fbf1be7a339b0e0
Jens Bissinger <mail@jens-bissinger.de>
2015-05-02 20:57:51 +0200

    Graceful perf. test.

--- test/performance/rollenspiel/role_grantee_performance_test.rb
+++ test/performance/rollenspiel/role_grantee_performance_test.rb
@@ -22,7 +22,7 @@ module Rollenspiel
         end
       end
 
-      expected_max_time = 0.1
+      expected_max_time = 0.2
 
       assert time < expected_max_time, "expected #{time} < #{expected_max_time}"
     end
